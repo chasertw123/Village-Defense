f7e061aa9b7fb58eb0e8564fada5918c2771e766
Chase Henderson <henderson.chase@gmail.com>
2014-12-05 18:42:09 -0500

    The start of something amazing.

---
+++ Village Defense/src/me/chasertw123/villagedefense/Main.java
@@ -0,0 +1,10 @@
+package me.chasertw123.villagedefense;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	public void onEnable() {
+		System.out.println("You are about to witness the evolution of something awesome.");
+	}
+}
