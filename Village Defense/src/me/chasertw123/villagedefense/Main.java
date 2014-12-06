5def4e4da56b071c34e4b45ed0fec8f0476e957c
Chase Henderson <henderson.chase@gmail.com>
2014-12-06 15:55:41 -0500

    Fixed Main

--- Village Defense/src/me/chasertw123/villagedefense/Main.java
+++ Village Defense/src/me/chasertw123/villagedefense/Main.java
@@ -1,9 +1,8 @@
 package me.chasertw123.villagedefense;
 
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin {
 
 	public void onEnable() {
 		System.out.println("You are about to witness the evolution of something awesome.");
