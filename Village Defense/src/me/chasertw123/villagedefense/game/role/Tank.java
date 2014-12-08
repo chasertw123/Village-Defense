e6a0ed01032f1e0fd3c12ad630bb882e2d929764
chasertw123 <henderson.chase@gmail.com>
2014-12-08 13:10:41 -0500

    Fixed Tank import

--- Village Defense/src/me/chasertw123/villagedefense/game/role/Tank.java
+++ Village Defense/src/me/chasertw123/villagedefense/game/role/Tank.java
@@ -10,13 +10,6 @@ import org.bukkit.block.banner.PatternType;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BannerMeta
 
-import me.chasertw123.villagedefense.utils.FancyItemStack;
-
-import org.bukkit.ChatColor;
-import org.bukkit.DyeColor;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
-
 public class Tank extends Role {
 
 	public Tank() {
